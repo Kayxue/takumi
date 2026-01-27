9216d3e33d272700dd6ee43ba2a7ec4250483fac
Kane Wang <kane@yeecord.com>
2026-01-27 12:43:11 +0800

    update dependencies

--- example/tanstack-start/vite.config.ts
+++ example/tanstack-start/vite.config.ts
@@ -1,10 +1,9 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
-  plugins: [viteTsConfigPaths(), tanstackStart(), viteReact()],
+  plugins: [viteTsConfigPaths(), tanstackStart()],
   ssr: {
     noExternal: ["@takumi-rs/core", "@takumi-rs/image-response"],
   },
